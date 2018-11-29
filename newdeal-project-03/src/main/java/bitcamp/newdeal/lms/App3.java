59ffbffa5c4e4f3cbecc4541f0d7c1f58cbe5772
codingbang <codingbang@gmail.com>
2018-11-29 09:03:45 +0900

    v03 HOME_WORK App3.java while문 추가

--- newdeal-project-03/src/main/java/bitcamp/newdeal/lms/App3.java
+++ newdeal-project-03/src/main/java/bitcamp/newdeal/lms/App3.java
@@ -7,42 +7,66 @@ public class App3 {
 
   public static void main(String[] args) {
     //출력 - 번호,내용,작성일,조회수
-    
+
     final int DEFAULT_SIZE = 20;
-    
+
     int[] no = new int[DEFAULT_SIZE];
     String[] contents = new String[DEFAULT_SIZE];
     Date[] writeDate = new Date[DEFAULT_SIZE];
     int[] viewCount = new int[DEFAULT_SIZE];
-    
+
     Scanner keyIn = new Scanner(System.in);
-    
+
     int len = 0;
-    
-    for (int i = 0; i < DEFAULT_SIZE; i++) {
+
+    //while문
+    while (len < DEFAULT_SIZE) {
       System.out.print("번호? ");
-      no[i] = Integer.parseInt(keyIn.nextLine());
-      
+      no[len] = Integer.parseInt(keyIn.nextLine());
+
       System.out.print("내용? ");
-      contents[i] = keyIn.nextLine();
-      
-      viewCount[i] = 0;
-      writeDate[i] = new Date(System.currentTimeMillis());
-      
+      contents[len] = keyIn.nextLine();
+
+      viewCount[len] = 0;
+      writeDate[len] = new Date(System.currentTimeMillis());
+
       len++;
-      
+
       System.out.println("계속 입력 하시겠습니까?(Y/n) ");
       String quitInput = keyIn.nextLine();
-      
+
       if (quitInput.equals("") || quitInput.equalsIgnoreCase("y")) {
-        
+
         continue;
       }
       break;
     }
-    
+
+    //for문
+    /*for (int i = 0; i < DEFAULT_SIZE; i++) {
+      System.out.print("번호? ");
+      no[i] = Integer.parseInt(keyIn.nextLine());
+
+      System.out.print("내용? ");
+      contents[i] = keyIn.nextLine();
+
+      viewCount[i] = 0;
+      writeDate[i] = new Date(System.currentTimeMillis());
+
+      len++;
+
+      System.out.println("계속 입력 하시겠습니까?(Y/n) ");
+      String quitInput = keyIn.nextLine();
+
+      if (quitInput.equals("") || quitInput.equalsIgnoreCase("y")) {
+
+        continue;
+      }
+      break;
+    }*/
+
     System.out.println();
-  //출력 - 번호,이름,이메일,전화,가입일
+    //출력 - 번호,이름,이메일,전화,가입일
     for (int i = 0; i < len; i++) {
       System.out.printf("번호: %d\n", no[i]);
       System.out.printf("내용: %s\n", contents[i]);
